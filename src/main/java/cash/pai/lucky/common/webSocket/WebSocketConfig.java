f8b5d5d3d511400a5b791cfcc0062891f2802c30
ldw <ldw1986hf123@163.com>
2020-08-20 12:29:40 +0800

    修复websocket不可用的问题

--- src/main/java/cash/pai/lucky/common/webSocket/WebSocketConfig.java
+++ src/main/java/cash/pai/lucky/common/webSocket/WebSocketConfig.java
@@ -4,6 +4,7 @@ import cash.pai.lucky.admin.config.websocket.MyEndpointConfigure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
 /**
  * WebSocket配置
  * 创建者  科帮网
@@ -11,12 +12,15 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  */
 @Configuration
 public class WebSocketConfig {
-/*
-    没发现用到，暂时先注释掉
+    /**
+     * 如果注释掉，启动虽然没有报错，但是会导致websocket不可用，
+     * 但是如果不注释掉，又导致单元测试跑不起来，原因待考
+     * @return
+     */
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
-    }*/
+    }
 
     /**
      * 支持注入其他类
